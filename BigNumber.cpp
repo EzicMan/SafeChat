082b3b276db427d1e59f0b877ebba317efda2b43
Александр <axew165@gmail.com>
2020-01-21 14:35:48 +0300

    Fixed String::operator*=

--- BigNumber.cpp
+++ BigNumber.cpp
@@ -139,10 +139,16 @@ bool String::operator!=(const String& right) const {
 }
 
 String& String::operator*=(int times) {
+	if (times < 0) {
+		throw std::invalid_argument("Can't multiply by negative numbers");
+	}
 	char* oldString = string;
 	string = new char[sSize * times + 1];
+	if (times == 0) {
+		string[0] = '\0';
+	}
 	for (int i = 0; i < times; i++) {
-		memcpy(string + i, oldString, sSize + 1);
+		memcpy(string + i * sSize, oldString, sSize + 1);
 	}
 	delete[] oldString;
 	sSize = sSize * times;
